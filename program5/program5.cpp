39da3726dbe07624d8de40667883818cb516c9bd
taznimt <taznimanwar06@gmail.com>
2024-03-26 15:13:01 +0700

    create case2

--- program5/program5.cpp
+++ program5/program5.cpp
@@ -47,16 +47,26 @@ int main()
         cout << "5. keluar " << endl;
         cout << "===============" << endl;
         cout << "masukan menu pilihan" << endl;
-        cout << pilihan;
-	}
+        cin >> pilihan;
+    
 
-    switch (pilihan)
-
+        switch (pilihan)
+        {
         case 1:
             inputData();
             cout << "Hasil penjumlahan = " << penjumlahan(bilangan1, bilangan2) << endl;
             break;
+        case 2:
+            inputData();
+            cout << "Hasil pengurangan = " << pengurangan(bilangan1, bilangan2) << endl;
+            break;
+        }
 
+       
+	}
+
+
+    
        
 }
 
