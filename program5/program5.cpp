3be894c7a602315694c3c68879933eb80a8e643e
taznimt <taznimanwar06@gmail.com>
2024-03-26 14:55:46 +0700

    create fungsi pengurangan

--- program5/program5.cpp
+++ program5/program5.cpp
@@ -19,24 +19,14 @@ int penjumlahan(int a, int b)
 	return a + b;
 }
 
+int pengurangan(int a, int b)
+{
+	return a - b;
+}
+
 int main()
 {
-	int pilihan;
-
-	do
-	{
-		cout << "Kalkulator Sederhana" << endl;
-		cout << "====================" << endl;
-		cout << "1. Penjumlahan" << endl;
-		cout << "2. Pengurangan" << endl;
-		cout << "3. Perkalian" << endl;
-		cout << "4. Pembagian" << endl;
-		cout << "5. Keluar" << endl;
-		cout << "====================" << endl;
-		cout << "Masuan menu pilihan : " << endl;
-		cin >> pilihan;
-	}
-
+	
 }
 
 
