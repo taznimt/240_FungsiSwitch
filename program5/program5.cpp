f9d6a8876219dab660693ae4701f5cca871841ed
taznimt <taznimanwar06@gmail.com>
2024-03-26 14:39:58 +0700

    create inputData

--- program5/program5.cpp
+++ program5/program5.cpp
@@ -6,6 +6,14 @@ using namespace std;
 
 int bilangan1, bilangan2;
 
+void inputData()
+{
+	cout << "Masukan bilangan pertama : ";
+	cin >> bilangan1;
+	cout << "Masukan bilangan kedua : ";
+	cin >> bilangan2;
+}
+
 int main()
 {
    
