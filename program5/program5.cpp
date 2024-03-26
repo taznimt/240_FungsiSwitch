a5143fd6a12abcada94978f62da170c63a2fb65f
taznimt <taznimanwar06@gmail.com>
2024-03-26 15:23:15 +0700

    create default

--- program5/program5.cpp
+++ program5/program5.cpp
@@ -70,6 +70,10 @@ int main()
             break;
         case 5:
             break;
+        
+        default:
+            cout << "Pilihan tidak ada " << endl;
+            break;
            
         }
 
