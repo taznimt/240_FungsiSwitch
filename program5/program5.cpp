82b0abd81af80f3461100fc0b583810762fbbbfc
taznimt <taznimanwar06@gmail.com>
2024-03-26 15:14:46 +0700

    create case perkalian

--- program5/program5.cpp
+++ program5/program5.cpp
@@ -60,6 +60,10 @@ int main()
             inputData();
             cout << "Hasil pengurangan = " << pengurangan(bilangan1, bilangan2) << endl;
             break;
+        case 3:
+            inputData();
+            cout << "Hasil perkalian = " << perkalian(bilangan1, bilangan2) << endl;
+            break;
         }
 
        
